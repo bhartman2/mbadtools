cf792d23bf547ddae0795b7cf2a1da99e08ebdad
Bruce Hartman <bruce@ahartman.net>
2026-02-25 17:36:44 -0700

    Add files via upload

---
+++ mbad_standard_packages.R
@@ -0,0 +1,10 @@
+#' mbad_standard_packages
+#'
+#' A character vector of the names of standard packages for MBAD
+#'
+#' @format ## `mbad_standard_packages`
+#' \describe{
+#'   \item{mbad_standard_packages}{standard package names in character form}
+#' }
+#' @source Available from Author
+"mbad_standard_packages"
